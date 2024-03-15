3361b4bd87455f0ebbbb9e6e6464780a30bd7f65
diego64 <diegoferreira1964@gmail.com>
2024-03-15 10:48:10 -0300

    Rotas: Ações no pedido

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,11 @@ import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getOrderDetails } from './routes/get-order-details'
+import { approveOrder } from './routes/approve-order'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -14,6 +19,11 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurant)
+  .use(getOrderDetails)
+  .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
